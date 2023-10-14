f4fd3b963d6fd0ce19a38aadcce48eba0cab2569
Ajith Anand <ajithanand303@gmail.com>
2023-10-14 21:26:28 +0530

    sidebar

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Sidebar.css'
+
+
+function Sidebar() {
+  return (
+    <div className='container'>
+      <aside>
+        <div className="toggle">
+          <div className="logo">
+            <img src="images/dashboard.png" />
+            <h2 className='primary'>Admin</h2>
+          </div>
+
+          <div className="close" id='close-btn'>
+            <span className="material-symbols-outlined">
+              close
+            </span>
+          </div>
+        </div>
+
+        <div className="sidebar">
+
+          <a href="#" className='active'>
+            <span className="material-symbols-outlined">
+              dashboard
+            </span>
+            <h3>Dashboard</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              person_outline
+            </span>
+            <h3>User</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              receipt_long
+            </span>
+            <h3>History</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              insights
+            </span>
+            <h3>Analytics</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              mail_outline
+            </span>
+            <h3>Tickets</h3>
+            <span className="message-count">10</span>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              inventory
+            </span>
+            <h3>Sale list</h3>
+          </a>
+          
+          <a href="#">
+            <span className="material-symbols-outlined">
+              report
+            </span>
+            <h3>Reports</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              settings
+            </span>
+            <h3>Setting</h3>
+          </a>
+
+          <a href="#">
+            <span className="material-symbols-outlined">
+              logout
+            </span>
+            <h3>Logout</h3>
+          </a>
+        </div>
+      </aside>
+    </div>
+  )
+}
+
+export default Sidebar
