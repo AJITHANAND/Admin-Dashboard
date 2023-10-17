e4923bfbc8a611c53a2de77453578f760678985b
Ajith Anand <ajithanand303@gmail.com>
2023-10-17 21:14:23 +0530

    top Navbar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+
+function Header() {
+    return (
+        <>
+            <div className='top-navBar'>
+                <h1>Dashboard</h1>
+                <h1>user</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
