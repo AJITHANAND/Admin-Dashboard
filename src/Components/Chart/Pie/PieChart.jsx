3a5873f6e60e29c9dc81d3a7446c11160a5b6fad
Ajith Anand <ajithanand303@gmail.com>
2023-11-06 17:32:49 +0530

    chart background change

--- src/Components/Chart/Pie/PieChart.jsx
+++ src/Components/Chart/Pie/PieChart.jsx
@@ -6,18 +6,20 @@ export const options = {
   // title: "Visit by Traffic Types",
   legend: "none",
   pieSliceText: "label",
+  backgroundColor: 'transparent',
   chartArea: {
     width: '95%', 
-    height: '90%'
+    height: '90%',
   },
   is3D: true,
 };
 
 function PieChart() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop:'0' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop:'0',backgroundColor:'transparent' }}>
       <Chart
         chartType="PieChart"
+        backgroundColor="transparent"
         data={data}
         options={options}
         width={"100%"}
